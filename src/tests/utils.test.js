556c80d9d83832b482c853ebfeb3e519aea6bcbe
MGRayd <mark.j.g@gmx.com>
2025-01-08 15:46:11 +0000

    removed depracted folders

--- src/tests/utils.test.js
+++
@@ -1,8 +0,0 @@
-// Add unit tests for critical functionality
-describe('calculateTotalScore', () => {
-  it('should calculate correct score with painted bonus', () => {
-    const roundPoints = [1, 2, 3, 4, 5];
-    const paintedBonus = true;
-    expect(calculateTotalScore(roundPoints, paintedBonus)).toBe(16);
-  });
-}); 
